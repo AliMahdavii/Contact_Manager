62e8b736898fa2354dab9e318f1a1373d7e91fed
Ali <84a.mahdaviniya@gmail.com>
2026-08-10 10:47:37 +0330

    added main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+y = ""
+for x in range(30):
+    y += "="
+
+print(y, "\n", "      CONTACT MANAGER", "\n", y)
+
+print("\n",
+      "1. Add Contact\n",
+      "2. Show Contacts\n",
+      "3. Search Contact\n",
+      "4. Edit Contact\n",
+      "5. Delete Contact\n",
+      "6. Exit\n"
+      )
+
+option = input("Choose an option: ")
